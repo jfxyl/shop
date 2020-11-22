a70cb8499dfa62e9e0204a117d299744fd17addb
vagrant <vagrant@homestead>
2020-11-22 12:12:20 +0000

    封装业务代码

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -10,62 +10,19 @@ use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
+use App\Services\CartService;
+use App\Services\OrderService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class OrdersController extends Controller
 {
-    public function store(OrderRequest $request){
+    public function store(OrderRequest $request,OrderService $orderService){
         $user = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
 
-        $order = DB::transaction(function()use($user,$request){
-            $address = UserAddress::find($request->address_id);
-            $address->update(['last_used_at' => Carbon::now()]);
-
-            $order = new Order([
-                'address' => [
-                    'address' => $address->full_address,
-                    'zip' => $address->zip,
-                    'contact_name' => $address->contact_name,
-                    'contact_phone' => $address->contact_phone,
-                ],
-                'remark' => $request->remark,
-                'total_amount' => 0
-            ]);
-            $order->user()->associate($user);
-            $order->save();
-
-            $totalAmount = 0;
-
-            foreach($request->items as $item){
-                $sku = ProductSku::find($item['sku_id']);
-
-                $orderItem = $order->items()->make([
-                    'amount' => $item['amount'],
-                    'price' => $sku->price,
-                ]);
-
-                $orderItem->product()->associate($sku->product_id);
-                $orderItem->productSku()->associate($sku);
-                $orderItem->save();
-                $totalAmount += $sku->price;
-
-                if ($sku->decreaseStock($item['amount']) <= 0) {
-                    throw new InvalidRequestException('该商品库存不足');
-                }
-            }
-
-            // 更新订单总金额
-            $order->update(['total_amount' => $totalAmount]);
-
-            $skuIds = collect($request->items)->pluck('sku_id');
-            $user->cartItems()->whereIn('product_sku_id',$skuIds)->delete();
-            return $order;
-        });
-
-        $this->dispatch(new CloseOrder($order,config('app.order_ttl')));
-        return $order;
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
     }
 
     public function index(Request $request)
