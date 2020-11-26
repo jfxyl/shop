d44d7d7be1ea5f4e9936e2da86122147d7c683cb
vagrant <vagrant@homestead>
2020-11-26 15:30:35 +0000

    完善假数据填充

--- database/factories/OrderItemFactory.php
+++ database/factories/OrderItemFactory.php
@@ -6,7 +6,17 @@ use App\Models\OrderItem;
 use Faker\Generator as Faker;
 
 $factory->define(OrderItem::class, function (Faker $faker) {
+    $product = \App\Models\Product::query()->where('on_sale',true)->inRandomOrder()->first();
+
+    $sku = $product->skus()->inRandomOrder()->first();
+
     return [
-        //
+        'amount'         => random_int(1, 5), // 购买数量随机 1 - 5 份
+        'price'          => $sku->price,
+        'rating'         => null,
+        'review'         => null,
+        'reviewed_at'    => null,
+        'product_id'     => $product->id,
+        'product_sku_id' => $sku->id,
     ];
 });
