29a06e8626f6a85af2cce464e65c2c25037108f6
vagrant <vagrant@homestead>
2020-12-06 09:04:36 +0000

    开发环境加入 Ngrok

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,3 +2,10 @@
 function route_class(){
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function ngrok_url($routeName,$params = []){
+    if(app()->environment('local') && $url = config('app.ngrok_url')){
+        return $url . route($routeName,$params,false);
+    }
+    return route($routeName,$params);
+}
